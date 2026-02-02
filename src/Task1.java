48eab7c8ca0899540068f19611b329fc5928c7d8
Aigerim09qi <aigerim.dzhunusheva@alatoo.edu.kg>
2026-02-02 14:05:07 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Task1 {
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<>(List.of("Book A", "Book B", "Book C"));
+        ArrayList<String> b = new ArrayList<>(List.of("Book B", "Book D"));
+
+        Set<String> set = new LinkedHashSet<>();
+        set.addAll(a);
+        set.addAll(b);
+
+        ArrayList<String> result = new ArrayList<>(set);
+        System.out.println(result);
+    }
+}
+
