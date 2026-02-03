70b08a73d879a8101b776fbcff96020fcc2e99b4
Aigerim09qi <aigerim.dzhunusheva@alatoo.edu.kg>
2026-02-03 10:41:46 +0300

    Add Task10

---
+++ src/Task10.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Task10 {
+    public static void main(String[] args) {
+        Integer[] nums = {8, 6, 4, 2};
+
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(nums));
+
+        System.out.println(list);
+    }
+}
+
