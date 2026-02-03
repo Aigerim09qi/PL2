6a1ace05e8c64545ea0297cb00f9c30ebefc5751
Aigerim09qi <aigerim.dzhunusheva@alatoo.edu.kg>
2026-02-03 12:05:37 +0300

    Add Task15

---
+++ src/Task15.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Task15 {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>(List.of(1,2,3,4,5,6));
+
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
+
+        for (int n : nums) {
+            if (n % 2 == 0) even.add(n);
+            else odd.add(n);
+        }
+
+        System.out.println("Even: " + even);
+
+        System.out.println("Odd: " + odd);
+    }
+}
